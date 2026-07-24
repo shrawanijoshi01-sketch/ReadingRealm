8dcabb8aabb6ebf1aae68a9ee970ca8643af0acc
Shrawani Joshi <shrawanijoshi01@gmail.com>
2026-07-24 23:37:36 +0530

    Update database config and register page

--- database.py
+++ database.py
@@ -3,5 +3,17 @@ import psycopg2
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-conn = psycopg2.connect(DATABASE_URL)
+if DATABASE_URL:
+    # Render
+    conn = psycopg2.connect(DATABASE_URL)
+else:
+    # Local PostgreSQL
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        database="Readingrealm",
+        user="postgres",
+        password="admin@123"   # <-- Replace with your password
+    )
+
 cursor = conn.cursor()
