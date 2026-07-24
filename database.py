d96536628ba23141fc2c8dc42cc854660f194a51
Shrawani Joshi <shrawanijoshi01@gmail.com>
2026-07-24 12:45:54 +0530

    Refactor database connection to use DATABASE_URL

--- database.py
+++ database.py
@@ -1,11 +1,7 @@
+import os
 import psycopg2
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="Readingrealm",
-    user="postgres",
-    password="admin@123",
-    port="5432"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
